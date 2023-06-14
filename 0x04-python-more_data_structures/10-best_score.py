546c12d72dd04e031310dbcdd7c0353d1081bcea
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-06-14 12:34:38 -0700

    0x04

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,11 @@
 def best_score(a_dictionary):
     if a_dictionary:
         b = sorted(a_dictionary.values())
-        return b[-1]
+
+        key_list = list(a_dictionary.keys())
+        val_list = list(a_dictionary.values())
+        position = val_list.index(b[-1])
+
+        return key_list[position]
     else:
         return None
