dfcdd370798c3203a3490299f2e1d6a0b0c8369b
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-07-03 11:24:59 -0700

    0x08

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+an module that have an class -Rectangle- that defines a rectangle
+"""
+
+class Rectangle:
+    """ a class that defines a rectangle """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        
+    @property    
+    def width(self):
+        """ width geter """
+        return self.__width
+    
+    @property
+    def height(self):
+        """ height geter """
+        return self.__height
+    
+    @width.setter
+    def width(self, value):
+        """ width setter """
+        if type(value) == int:
+            if value >= 0:
+                self.__width = value
+            else:
+                raise ValueError("width must be >= 0")
+        else:
+            raise TabError("width must be an integer")
+
+    
+    @height.setter
+    def height(self, value):
+        """ height setter """
+        if type(value) == int:
+            if value >= 0:
+                self.__height = value
+            else:
+                raise ValueError("height must be >= 0")
+        else:
+            raise TabError("height must be an integer")
