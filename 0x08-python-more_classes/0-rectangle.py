82ace71f3c768b704389b0c94302b69ba5e02519
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-07-03 08:57:46 -0700

    0x08

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+an module that have an empty class -Rectangle- that defines a rectangle
+"""
+class Rectangle:
+    """ an empty class that defines a rectangle """
+    pass
