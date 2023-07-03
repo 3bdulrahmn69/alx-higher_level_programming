cc4232661104da61ac7f41473230b0f0061e4545
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-07-03 14:25:52 -0700

    0x08

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -11,6 +11,10 @@ class Rectangle:
         self.width = width
         self.height = height
 
+    def __del__(self):
+        """prints a string when an instance has been deleted"""
+        print("Bye rectangle...")
+
     @property
     def width(self):
         """getter for the private instance attribute width"""
@@ -40,17 +44,17 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """Public instance method that returns the rectangle area"""
+        """returns the area of the rectangle"""
         return self.__width * self.__height
 
     def perimeter(self):
-        """Public instance method that returns the rectangle perimeter"""
+        """returns the perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width * 2) + (self.__height * 2)
 
     def __str__(self):
-        """print the rectangle with the character #"""
+        """returns printable string representation of the rectangle"""
         string = ""
         if self.__width != 0 and self.__height != 0:
             string += "\n".join("#" * self.__width
@@ -58,9 +62,5 @@ class Rectangle:
         return string
 
     def __repr__(self):
-        """return a string representation of the rectangle to be able to recreate a new instance by using eval"""
+        """returns a string representation of the rectangle for reproduction"""
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
-    
-    def __del__(self):
-        """Print the message -Bye rectangle...- when an instance of Rectangle is deleted"""
-        print("Bye rectangle...")
