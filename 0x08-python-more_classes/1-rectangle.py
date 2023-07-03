fb158533141cf9ad5ea360fed5c7934ac855754c
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-07-03 12:38:58 -0700

    0x08

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,15 +8,14 @@ class Rectangle:
     """ a class that defines a rectangle """
     def __init__(self, width=0, height=0):
         """Initializes the rectangle"""
-        self.width = width
         self.height = height
-        
+        self.width = width
+
     @property
     def width(self):
         """getter for the private instance attribute width"""
         return self.__width
-    
-    
+
     @width.setter
     def width(self, value):
         """setter for the private instance attribute width"""
@@ -27,7 +26,7 @@ class Rectangle:
                 raise ValueError("width must be >= 0")
         else:
             raise TypeError("width must be an integer")
-        
+
     @property
     def height(self):
         """getter for the private instance attribute height"""
