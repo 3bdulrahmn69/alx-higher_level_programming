7a6feeb6f3dff1f1bcf4f62770159703dea603ba
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-07-10 08:54:16 -0700

    0x0A

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -3,8 +3,36 @@
 module that contains a class Square that inherits from Rectangle
 """
 
+class BaseGeometry:
+    """ a class"""
+    def area(self):
+        """raises an Exception"""
+        raise Exception("area() is not implemented")
 
-Rectangle = __import__('9-rectangle').Rectangle
+    def integer_validator(self, name, value):
+        """validates value"""
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    """a class that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        """Instantiation"""
+        super().__init__()
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """area method implemented"""
+        return self.__height * self.__width
+
+    def __str__(self):
+        """rectangle description"""
+        return f"[Rectangle] {self.__width}/{self.__height}"
 
 
 class Square(Rectangle):
