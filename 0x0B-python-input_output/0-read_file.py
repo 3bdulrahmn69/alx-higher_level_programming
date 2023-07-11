9ad1cd95acdfcd893168738b60b55dff78d2338e
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-07-11 07:56:20 -0700

    0x0B

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+a module that have a function that reads a text file and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """a function that reads a text file"""
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+
+        print(read_data)
+
+    f.closed
