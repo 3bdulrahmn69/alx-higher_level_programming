a31a0990270f3995be2078362b353fbede49f2e3
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-07-17 14:42:31 -0700

    0x0C

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,40 +1,36 @@
 #!/usr/bin/python3
-"""
-Base module class
-"""
+"""Base module class"""
 import json
 
 
 class Base:
     """
     Base class
-
     Attribute:
         __nb_object : private class attribute
         id          : public instance attribute
     """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
         """
-        Initialize class constructor
-
+        Initialize class constructor.
         Args:
-            id (int): a unique identifier of base class
+            id (int): a unique identifier of base class.
         """
-        if id == None:
-            Base.__nb_objects +=1
-            self.id = Base.__nb_objects
-        else:
+        if id is not None:
             self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
         """
-        Return the JSON serialization of a list of dicts
-
+        Return the JSON serialization of a list of dicts.
         Args:
-            list_dictionaries (list): A list of dictionaries
+            list_dictionaries (list): A list of dictionaries.
         """
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
@@ -43,10 +39,9 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         """
-        Write the JSON serialization of a list of objects to a file
-
+        Write the JSON serialization of a list of objects to a file.
         Args:
-            list_objs (list): A list of inherited Base instances
+            list_objs (list): A list of inherited Base instances.
         """
         filename = cls.__name__ + ".json"
         with open(filename, "w") as jsonfile:
@@ -59,16 +54,14 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """
-        Return the deserialization of a JSON string
-
+        Return the deserialization of a JSON string.
         Args:
-            json_string (str): A JSON str representation of a list of dicts
-
+            json_string (str): A JSON str representation of a list of dicts.
         Returns:
-            If json_string is None or empty - an empty list
-            Otherwise - the Python list represented by json_string
+            If json_string is None or empty - an empty list.
+            Otherwise - the Python list represented by json_string.
         """
-        if json_string == None or json_string == "[]":
+        if json_string is None or json_string == "[]":
             return []
         return json.loads(json_string)
 
@@ -76,10 +69,9 @@ class Base:
     def create(cls, **dictionary):
         """
         Return:
-            a class instantied from a dictionary of attributes
-
+            a class instantied from a dictionary of attributes.
         Args:
-            **dictionary (dict): Key/value pairs of attributes to initialize
+            **dictionary (dict): Key/value pairs of attributes to initialize.
         """
         if dictionary and dictionary != {}:
             if cls.__name__ == "Rectangle":
@@ -92,9 +84,8 @@ class Base:
     @classmethod
     def load_from_file(cls):
         """
-        Return a list of classes instantiated from a file of JSON strings
-        Reads from `<cls.__name__>.json`
-
+        Return a list of classes instantiated from a file of JSON strings.
+        Reads from `<cls.__name__>.json`.
         Returns:
             If the file does not exist - an empty list.
             Otherwise - a list of instantiated classes.
